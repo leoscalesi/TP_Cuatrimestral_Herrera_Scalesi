d49882281aa4750fcd7bbf412a5b541684fe05e9
rah536 <raherrera536@gmail.com>
2022-06-13 21:56:53 -0300

    Version8. Se agregan clases en las capas AccesoDatos y Negocio, y se genera nuevo formulario en la capa de Vistas para conseguir la lectura de la base de datos. El formulario VerRoles trae desde dbRoles la tabla con id y nombre.

---
+++ TP_Cuatrimestral_Herrera_Scalesi/Negocio/RolNegocio.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+using AccesoDatos;
+
+namespace Negocio
+{
+    public class RolNegocio
+    {
+        public List<Rol> listar()
+        {
+            List<Rol> lista = new List<Rol>();
+            AccesoaDatos datos = new AccesoaDatos();
+
+            try
+            {
+
+                datos.setearConsulta("select * from Roles");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+
+                    Rol aux = new Rol();
+
+                    aux.id = (int)datos.Lector["id"];
+                    aux.nombre = (string)datos.Lector["nombre"];
+                  
+
+                    lista.Add(aux);
+
+                }
+
+                return lista;
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+        }
+
+
+    }
+}
